70097abe1242b2bfe62091bdf17ea672772b987b
Rupam-It <111190901+Rupam-It@users.noreply.github.com>
2023-04-25 11:16:59 +0530

    python

---
+++ w_msg.py
@@ -0,0 +1,19 @@
+# this is a project for send msg to whatsapp contact 
+import pywhatkit as pwk
+pwk.sendwhatmsg("+916003008001",'hey i have a doubt',13,40)
+#this is doubt not number
+
+
+
+
+print("way to send image")
+# pwk.sendwhats_image("Group_Name", "Media/image.png", "Hi")
+ 
+# pwk.sendwhats_image("Name", "Media/images.png")
+
+
+print("send msg to the group")
+#pwk.sendwhatmsg_to_group("Group_Name", "Hey Guys! How's everybody?", 11, 0)
+
+import keyboard as k 
+
---
+++ whatsapp_spam_msg.py
@@ -0,0 +1,9 @@
+import pyautogui as p
+import time
+time.sleep(10)
+count=0
+while(count<1250):
+    p.typewrite("vii valo a6is")
+    p.press("enter")
+    count+=1
+ 
